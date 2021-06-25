bb4b858d26ecae542f28bc6c2722173c9387e038
VitoTel <vito@tel25.com>
2021-06-25 17:43:41 +0800

    feat: add restart button

--- src/index.js
+++ src/index.js
@@ -24,38 +24,14 @@ const Square = ({ value, onClick }) => {
   );
 }
 
-const Board = () => {
-  const [isCirclePlayer, setIsCirclePlayer] = useState(false)
-  const [playLog, setPlayLog] = useState([])
-  console.log('Board playLog: ', playLog);
-
-  const checkWin = (pValue) => {
-    const indexList = playLog.filter(item => item.value === pValue).map(item => item.index)
-    const isWin = winCases.some(winCase => winCase.every(num => indexList.some(ii => ii === num)))
-    return isWin
-  }
-
-  const generateWinner = () => {
-    if (checkWin(CIRCLE_PLAYER_VALUE)) {
-      return CIRCLE_PLAYER_VALUE
-    } else if (checkWin(CROSS_PLAYER_VALUE)) {
-      return CROSS_PLAYER_VALUE
-    }
-    return null
-  }
-
-  const playerValue = isCirclePlayer ? CIRCLE_PLAYER_VALUE : CROSS_PLAYER_VALUE
-  const winner = generateWinner()
-  const gameStatus = winner ? `Game over, ${winner} is winner` : `Next player: ${playerValue}`
-
+const Board = ({ isGameOver, playLog, onClickSquare }) => {
   const renderSquare = (index) => {
     const value = playLog.find(item => item.index === index)?.value
     const handleClick = () => {
-      if (value || winner) {
+      if (isGameOver || value) {
         return
       }
-      setPlayLog((prevLog) => ([ ...prevLog, { index, value: playerValue } ]))
-      setIsCirclePlayer(prev => !prev)
+      onClickSquare(index)
     }
 
     return (
@@ -65,7 +41,6 @@ const Board = () => {
 
   return (
     <div>
-      <div className="status">{gameStatus}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -86,14 +61,48 @@ const Board = () => {
 }
 
 const Game = () => {
+  const [isCirclePlayerRound, setIsCirclePlayerRound] = useState(false)
+  const [playLog, setPlayLog] = useState([])
+  console.log('Board playLog: ', playLog);
+
+  const checkWin = (pValue) => {
+    const indexList = playLog.filter(item => item.value === pValue).map(item => item.index)
+    const isWin = winCases.some(winCase => winCase.every(num => indexList.some(ii => ii === num)))
+    return isWin
+  }
+
+  const generateWinner = () => {
+    if (checkWin(CIRCLE_PLAYER_VALUE)) {
+      return CIRCLE_PLAYER_VALUE
+    } else if (checkWin(CROSS_PLAYER_VALUE)) {
+      return CROSS_PLAYER_VALUE
+    }
+    return null
+  }
+
+  const playerValue = isCirclePlayerRound ? CIRCLE_PLAYER_VALUE : CROSS_PLAYER_VALUE
+  const winner = generateWinner()
+  const gameStatus = winner ? `Game over, ${winner} is winner` : `Next player: ${playerValue}`
+
+  const handleClickReset = () => {
+    setPlayLog([])
+  }
+
+  const handleClickSquare = (index) => {
+    setPlayLog((prevLog) => ([ ...prevLog, { index, value: playerValue } ]))
+    setIsCirclePlayerRound(prev => !prev)
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board isGameOver={!!winner} playLog={playLog} onClickSquare={handleClickSquare} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div className="status">{gameStatus}</div>
+        {!!playLog.length && (
+          <button className="restart" onClick={handleClickReset}>{'Restart'}</button>
+        )}
       </div>
     </div>
   );
